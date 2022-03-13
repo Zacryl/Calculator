aca5553bcb43f7d1935d4d9724bfc429cf2dec2a
Adam <zacryldev@gmail.com>
2022-03-13 18:57:08 +0100

    Most methods and fail safes implemented

--- Program.cs
+++ Program.cs
@@ -3,26 +3,53 @@ using System.Collections.Generic;
 namespace Calculator.MainMenu{
 class MainClass{
     //Value after calculations.
-    int calcedNumber = 0;
-    private int initialNumber(){
+    private static int calcedNumber = 0;
+    private static void initialNumber(){
     //To replaced calcedNumber with an initial number.
         Console.WriteLine("Enter a number");
-        string newNumber = Console.ReadLine();
-        return Convert.ToInt32(newNumber);
+        string newNumber = Console.ReadLine(); 
+        if(!Calculator.Math.Calculations.CheckValidNumber(newNumber)){
+            initialNumber();
+        }else{
+            calcedNumber =  Convert.ToInt32(newNumber);
+        }
     }
-    static void Main(string[] args)
-    {
-        Console.Clear(); 
-        //Starts asking the user for what kind of calculations they want to do.
+    private static void Menu(){
+        //Asks the user for what kind of calculations they want to do.
         Console.WriteLine("Enter the following letters for the corresponding option"+ 
         "\n-a: Addition\n-s: Subtraction\n-m: Multiplication\n-d: Division");
         string input = Console.ReadLine();
         switch (input)
         {
-            case a:
-                
+            case "a":
+                calcedNumber = Calculator.Math.Calculations.Addition(calcedNumber);
+                Console.WriteLine("Calculation is: {0}", calcedNumber);
+                break;
+            case "s":
+                calcedNumber = Calculator.Math.Calculations.Subtraction(calcedNumber);
+                Console.WriteLine("Calculation is: {0}", calcedNumber);
+                break;
+            case "m":
+                calcedNumber = Calculator.Math.Calculations.Multiplication(calcedNumber);
+                Console.WriteLine("Calculation is: {0}", calcedNumber);
+                break;
+            case "d":
+                calcedNumber = Calculator.Math.Calculations.Division(calcedNumber);
+                Console.WriteLine("Calculation is: {0}", calcedNumber);
+                break;
             default:
+                Console.WriteLine("Sorry not an option");
+                Menu();
+                break;
         }
+    }
+    static void Main(string[] args)
+    {
+        /*Clears the console from any previous action and yellow errors as well as run initial
+        and menu methods*/
+        Console.Clear(); 
+        initialNumber();
+        Menu();
     }
 }    
 }
--- calculator/Math/Calculations.cs
+++ calculator/Math/Calculations.cs
@@ -3,29 +3,58 @@ using System.Collections.Generic;
 
 namespace Calculator.Math {
     public class Calculations{
+
+        public static bool CheckValidNumber(string input){
+            try
+            {
+                Convert.ToInt32(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a valid number");
+                return false;
+            }
+        }
         public static int Addition(int currNumber){
+            Console.WriteLine("Enter a number to add:");
             string input = Console.ReadLine();
-            int number = Convert.ToInt32(number);
-            currNumber += number;
-            return currNumber;
+            if(!CheckValidNumber(input)){
+                return Addition(currNumber);
+            }else{
+                int number = Convert.ToInt32(input);
+                return currNumber += number;
+            }
         }
         public static int Subtraction (int currNumber){
+            Console.WriteLine("Enter a number to subtract:");
             string input = Console.ReadLine();
-            int number = Convert.ToInt32(number);
-            currNumber -= number;
-            return currNumber;
+            if(!CheckValidNumber(input)){
+                return Subtraction(currNumber);
+            }else{
+                int number = Convert.ToInt32(input);
+                return currNumber += number;
+            }
         }
         public static int Multiplication (int currNumber){
+            Console.WriteLine("Enter a number to multiply by:");
             string input = Console.ReadLine();
-            int number = Convert.ToInt32(number);
-            currNumber *= number;
-            return currNumber;
+            if(!CheckValidNumber(input)){
+                return Multiplication(currNumber);
+            }else{
+                int number = Convert.ToInt32(input);
+                return currNumber += number;
+            }
         }
         public static int Division (int currNumber){
+            Console.WriteLine("Enter a number to divide by:");
             string input = Console.ReadLine();
-            int number = Convert.ToInt32(number);
-            currNumber /= number;
-            return currNumber;
+            if(!CheckValidNumber(input)){
+                return Division(currNumber);
+            }else{
+                int number = Convert.ToInt32(input);
+                return currNumber += number;
+            }
         }
     }
 }
